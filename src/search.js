fcce418f86720a263ca01ac271e4b00f03a1e0b1
Kristan <chransberry@gmail.com>
2018-11-05 22:35:04 -0700

    unsuccessfully trying to get the search to work...

--- src/search.js
+++ src/search.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import {Link} from "react-router-dom"
+import * as BooksAPI from "./BooksAPI"
+import Book from "./book.js"
 
 class Search extends React.Component {
+    state = {
+      books: [],
+      query: ""
+    };
+
+	searchBooks = async function(query) {
+    	let results = await BooksAPI.search(query);
+        console.log(results);
+    };
 	render() {
 		return (
 			
@@ -9,12 +20,14 @@ class Search extends React.Component {
             <div className="search-books-bar">
               <Link to="/" className="close-search">Close</Link>
               <div className="search-books-input-wrapper">
-              <input type="text" placeholder="Search by title or author"/>
+              <input type="text" placeholder="Search by title or author" onChange={e => {this.setState({query: this.value}); this.searchBooks(e.value)}}/>
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+{this.state.books.map(book => <Book key={book.id} book={book} onUpdateBooks={this.onUpdateBooks} />)}
+</ol>
             </div>
           </div>
         
