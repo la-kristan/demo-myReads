3da9ab4d3783d9a3486253afbd8fac1851fec3a0
Kristan <chransberry@gmail.com>
2018-11-05 22:33:27 -0700

    moved updateBooks() function

--- src/home.js
+++ src/home.js
@@ -1,44 +1,10 @@
 import React from 'react'
 import {Link} from "react-router-dom"
-import * as BooksAPI from "./BooksAPI"
 import Bookshelf from "./bookshelf.js"
 
 class Home extends React.Component {
-
-	constructor(props) {
-		super(props);
-		this.state = {
-			books: []
-		}
-	}
   
-  updateBooks = (book, shelf) => {
-    debugger;
-  	BooksAPI.update(book, shelf).then(
-      function (){
-      //copy list of books
-      let listCopy = this.state.books.slice(0);
-      //check to see if the book is in the list
-      //if not, add it to the list
-      let allIDs = listCopy.map(b => b.ID);
-       if(allIDs.indexOf(book.ID) === -1) {
-           listCopy.push(book);
-           }
-      //update the book with new shelf
-        for(let i = 0; i <= allIDs.length; i++) {
-        	if (book.ID === allIDs[i]) {
-            	listCopy[i].shelf = shelf;
-                //return;
-            }
-        }
-        //update state with the copied list
-        this.setState({books: listCopy});
-      })}
-
-	async componentDidMount() {
-		this.setState({books: await BooksAPI.getAll()});
-	}
-	render() {
+  render() {
 		return (
           <div className="list-books">
             <div className="list-books-title">
@@ -46,9 +12,9 @@ class Home extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <Bookshelf name="Currently Reading" books={this.state.books.filter(book => book.shelf === "currentlyReading")} onUpdateBooks={this.updateBooks} />
-                <Bookshelf name="Want to Read" books={this.state.books.filter(book => book.shelf === "wantToRead")} onUpdateBooks={this.updateBooks} />
-                <Bookshelf name="Read" books={this.state.books.filter(book => book.shelf === "read")} onUpdateBooks={this.updateBooks} />
+                <Bookshelf name="Currently Reading" books={this.props.books.filter(book => book.shelf === "currentlyReading")} onUpdateBooks={this.props.onUpdateBooks} />
+                <Bookshelf name="Want to Read" books={this.props.books.filter(book => book.shelf === "wantToRead")} onUpdateBooks={this.props.onUpdateBooks} />
+                <Bookshelf name="Read" books={this.props.books.filter(book => book.shelf === "read")} onUpdateBooks={this.props.onUpdateBooks} />
               </div>
             </div>
             <div className="open-search">
