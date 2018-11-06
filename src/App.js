d47358f0668190001decb3877f48e09e6a769b5a
Kristan <chransberry@gmail.com>
2018-11-05 22:31:54 -0700

    moved UpdateBooks() function

--- src/App.js
+++ src/App.js
@@ -5,29 +5,57 @@ import Search from "./search.js"
 import Home from "./home.js"
 import * as BooksAPI from "./BooksAPI"
 
-
-function updateShelf(book, shelf) {
-    BooksAPI.update(book, shelf).then(() =>
-      {book.shelf = shelf;
-      this.setState({book: book})
-      }
-      )
-    }
-
-
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  };
+  
+  //special thanks to Jason White (drunkenkismet [FEND] Project Coach) for his help with this function!
+  updateBooks = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(() => {
+      //copy list of books
+      let listCopy = this.state.books.slice(0);
+      //check to see if the book is in the list
+      //if not, add it to the list
+      let allIDs = listCopy.map(b => b.id);
+      if (allIDs.indexOf(book.id) === -1) {
+        listCopy.push(book);
+      }
+      //update the book with new shelf
+      for (let i = 0; i <= allIDs.length; i++) {
+        if (book.id === allIDs[i]) {
+          listCopy[i].shelf = shelf;
+          //return;
+        }
+      }
+      //update state with the copied list
+      this.setState({ books: listCopy });
+    });
+  };
+
+  async componentWillMount() {
+    await BooksAPI.getAll().then(res => {
+      this.setState({ books: res });
+    });
+  }
 
   render() {
     return (
       <div className="app">
         <Switch>
-          
-
-          <Route exact path={"/"} render={(props) => <Home updateShelf={updateShelf()} />} />
-          <Route exact path={"/search"} render={(props) => <Search updateShelf={updateShelf()} />} />
+          <Route
+            exact
+            path={"/"}
+            render={() => <Home onUpdateBooks={this.updateBooks} books={this.state.books}/>}
+          />
+          <Route
+            exact
+            path={"/search"}
+            render={() => <Search onUpdateBooks={this.updateBooks} shelfBooks={this.state.books}/>}
+          />
         </Switch>
       </div>
-    )
+    );
   }
 }
 
