d0542cd2a3c12764a1c58880daf3a5e55a0e073e
Kristan <chransberry@gmail.com>
2018-11-06 22:13:02 -0700

    fixed errors from missing book data

--- src/book.js
+++ src/book.js
@@ -14,7 +14,7 @@ class Book extends React.Component {
 			<li>
 				<div className="book">
 					<div className="book-top">
-						<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` || "" }}></div>
+						<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: this.props.book.imageLinks ? `url(${this.props.book.imageLinks.thumbnail})` : "" }}></div>
 						<div className="book-shelf-changer">
 							<select value={this.state.shelf} onChange={e => {this.props.onUpdateBooks(this.props.book, e.target.value); this.setState({shelf: e.target.value})}}>
 								<option value="move" disabled>Move to...</option>
@@ -25,8 +25,8 @@ class Book extends React.Component {
 							</select>
 						</div>
 					</div>
-					<div className="book-title">{this.props.book.title || "Untitled"}</div>
-					<div className="book-authors">{this.props.book.authors[0] || "No author"}</div>
+					<div className="book-title">{this.props.book.title ? this.props.book.title : "Untitled"}</div>
+					<div className="book-authors">{this.props.book.authors ? this.props.book.authors[0] : "No author"}</div>
 				</div>
 			</li>
 		);
